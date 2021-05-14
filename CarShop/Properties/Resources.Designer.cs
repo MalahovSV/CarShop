b80cb621314a2d379d3535f6881026b809c39c5e
malahovsv1999@gmail.com <malahovsv1999@gmail.com>
2021-05-14 13:39:49 +0700

    update

--- CarShop/Properties/Resources.Designer.cs
+++ CarShop/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace CarShop.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AutorisationForm.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CarShop.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
